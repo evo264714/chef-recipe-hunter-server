1ea7090c1b20a6a9a3b03fc484b9ba79d597431f
evo264714 <mh264714@gmail.com>
2023-05-03 03:22:18 +0600

    successfully loaded and fixed some server datas of chefs

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 2000;
+const cors = require('cors');
+
+const chefs = require('./data/chefs.json')
+
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('Chef is running')
 });
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
+})
 
 app.listen(port, () =>{
     console.log(`chef is running on port: ${port}`);
